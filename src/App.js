4823358fa4cfe11330e36e9c38d665fb210b8d08
Asma Ali <amino.asmaa.ali@gmail.com>
2020-12-22 21:54:51 -0500

    setup firebase

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    //BEM convention
+    <div className="app">
+      <h1> Let's get started! </h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDvQZH-wiblPztC-FyCOYwCx4jriVPEzfg",
+  authDomain: "clone-df399.firebaseapp.com",
+  projectId: "clone-df399",
+  storageBucket: "clone-df399.appspot.com",
+  messagingSenderId: "312166948459",
+  appId: "1:312166948459:web:a14d0a4f34ec46b822db2e",
+  measurementId: "G-6XKR7Z1SCN",
+};
