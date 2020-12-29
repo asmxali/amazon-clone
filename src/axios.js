636e61d3f5324690e67e57c6009dc672b376720c
Asma Ali <amino.asmaa.ali@gmail.com>
2020-12-28 21:29:04 -0500

    deployed functions to firebase

--- src/axios.js
+++ src/axios.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:5001/clone-df399/us-central1/api", //the API url (cloud function url)
+  baseURL: "https://us-central1-clone-df399.cloudfunctions.net/api",
+  // "http://localhost:5001/clone-df399/us-central1/api", //the API url (cloud)
 });
 export default instance;
